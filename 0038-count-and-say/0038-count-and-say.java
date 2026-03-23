ccd28994c25b483ad8aa947dbac4f7ed71965cda
Apoorva Marathi <165326786+apoorvamarathi@users.noreply.github.com>
2026-03-23 17:40:31 +0530

    Time: 16 ms (10.98%), Space: 47.3 MB (8.43%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String countAndSay(int n) {
+        String result = "1";
+        for(int i=2;i<=n;i++){
+            String temp="";
+            int count=1;
+            for(int j=1;j<result.length();j++){
+                if(result.charAt(j)==result.charAt(j-1)){
+                    count++;
+                }else{
+                    temp += count + "" + result.charAt(j - 1);
+   count = 1;             }
+            }
+            temp += count + "" + result.charAt(result.length()-1);
+            result = temp;
+        }
+        return result;
+    }
+}
